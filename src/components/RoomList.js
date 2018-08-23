b50c719b0101387464b22b5b79f030aeac4ee508
Lauren Yeager <Lauren.e.yeager@gmail.com>
2018-08-23 15:23:31 -0400

    List Chat Rooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -20,7 +20,7 @@ class RoomList extends Component {
 
   render() {
     const roomList = this.state.rooms.map((room) =>
-      <li key= {room.key}>{room.name}</li>
+      <li key = {room.key}>{room.name}</li>
     );
 
     return(
